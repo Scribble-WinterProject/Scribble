a8f999117a7348525dcf101a70df2495dcc4cff2
Harsh Rai <htc38199@gmail.com>
2024-03-25 16:54:58 +0530

    completed pdf deletinn

--- client/src/components/PDF/PdfCard.jsx
+++ client/src/components/PDF/PdfCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../../../public/style.css';
 import { useNavigate } from 'react-router';
+import { deletePdfById } from '../../appwrite/api';
 
 function PdfCard({ note }) {
     const [pdfs, setpdfs] = useState([]);
@@ -18,13 +19,26 @@ function PdfCard({ note }) {
     navigate(`/pdfviewer/${id}`);
   };
 
+  const handlePdfDelete = async (id) => {
+    try {
+      await deletePdfById(id);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
     return (
         <div className='pdfCard'>
             <p className='pdfCardName' onClick={handleNoteTitleClick} >{note.title}</p>
             <div>
       {pdfs.map((pdf, index) => (
-        <div key={pdf.$id} onClick={() => handlePdfClick(pdf.$id)} className="pdfOnAllPdfs">
-          <p>PDF {index + 1}</p>
+        <div key={pdf.$id}  className="pdfOnAllPdfs">
+          <p className='pdfName'>PDF {index + 1}</p>
+          <div className='pdfButtons'>
+            <button onClick={() => handlePdfClick(pdf.$id)} className='pdfButton'>View</button>
+            <button onClick={()=> handlePdfDelete(pdf.$id)} className='pdfButton'>Delete</button>
+          </div>
         </div>
       ))}
             </div>
